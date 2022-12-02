c47b6b76a88dda7e467dc9b634e39b95400956d1
Ezequiel <epuyosdacosta@gmail.com>
2022-12-02 18:12:04 +0100

    Contacts model controller and views

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,4 @@
 class Contact < ApplicationRecord
   belongs_to :user
+  validates :contact_type, inclusion: { in: %w(Garante Inquilino Propietario Proveedor) }
 end
---
+++ db/migrate/20221202165447_change_column_name_to_contact.rb
@@ -0,0 +1,5 @@
+class ChangeColumnNameToContact < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :contacts, :type, :contact_type
+  end
+end
