d2c1100240bc5132ffb8a50440fc51c45635a89c
Ezequiel <epuyosdacosta@gmail.com>
2022-12-02 16:21:44 +0100

    authentication update

---
+++ db/migrate/20221202151758_change_data_type_to_users.rb
@@ -0,0 +1,5 @@
+class ChangeDataTypeToUsers < ActiveRecord::Migration[7.0]
+  def change
+    change_column :users, :phone, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_02_150348) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_02_151758) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,7 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_02_150348) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.integer "phone"
+    t.string "phone"
     t.string "role"
     t.string "agency_name"
     t.index ["email"], name: "index_users_on_email", unique: true
