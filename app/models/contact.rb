fd28404e962f51900f4ed2d0893ec4c3119f83d7
Ezequiel <epuyosdacosta@gmail.com>
2022-12-03 12:55:20 +0100

    Test property forms

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,8 @@
 class Contact < ApplicationRecord
   belongs_to :user
   validates :kind, inclusion: { in: %w(Garante Inquilino Propietario Proveedor) }
-  validates_presence_of :first_name, :last_name
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :phone, numericality: true
+  validates_presence_of :first_name
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, allow_blank: true
+  validates :phone, numericality: true, allow_blank: true
   has_many :properties
 end
