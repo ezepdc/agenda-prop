af24bc7269c80dfe0c43a4f4166b3d425e0e3302
Ezequiel <epuyosdacosta@gmail.com>
2022-12-06 18:59:16 +0100

    owneradd propietario to form properties

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,6 +4,6 @@ class Contact < ApplicationRecord
   validates_presence_of :first_name
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, allow_blank: true
   validates :phone, numericality: true, allow_blank: true
-  has_many :properties
+  has_many :properties, foreign_key: 'owner_id'
   has_many :contracts
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :contacts
-  validates :phone, numericality: true
+  validates :phone, numericality: true, allow_blank: true
   validates :first_name, presence: true
   validates :agency_name, presence: true
   has_many :properties
