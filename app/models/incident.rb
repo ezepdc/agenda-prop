88ceb425f6fedcc3863ea76d60f9f5561add8f95
Ezequiel <epuyosdacosta@gmail.com>
2022-12-07 14:46:17 +0100

    incidents view added

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -2,4 +2,6 @@ class Incident < ApplicationRecord
   belongs_to :property
   belongs_to :contact
   belongs_to :user
+  validates :kind, presence: true
+  validates :description, presence: true
 end
