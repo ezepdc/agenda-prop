3fdfdeb494c1c69c82e5c43264d8fb21687fb085
Ezequiel <epuyosdacosta@gmail.com>
2022-12-08 14:00:04 +0100

    Documents attach added to Contracts

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -5,4 +5,9 @@ class Contract < ApplicationRecord
   belongs_to :user
   validates :kind, presence: true
   has_many :contract_prices, dependent: :destroy
+  has_many_attached :guarantor_documents
+  has_many_attached :guarantee_documents
+  has_many_attached :security_deposit_receipts
+  has_many_attached :security_deposit_return_receipts
+  has_many_attached :contract_documents
 end
