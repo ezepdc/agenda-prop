93e9623b133c43827f4f57c293422b87988afbcf
Ezequiel <epuyosdacosta@gmail.com>
2022-12-08 19:19:14 +0100

    dashboard view added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get "dashboard", to: "dashboard#index"
   resources :contacts
   resources :properties
   resources :contracts do
