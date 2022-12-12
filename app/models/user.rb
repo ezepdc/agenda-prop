624cef63dbb401d8345c5584f860c95f95efdc7f
Ezequiel <epuyosdacosta@gmail.com>
2022-12-12 15:54:40 +0100

    Back button in views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable, :registerable
+  # :registerable
   has_many :contacts
   validates :phone, numericality: true, allow_blank: true
   validates :first_name, presence: true
